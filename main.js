dbb37dbbfa02f81d8d961618e1271a5f4f0297a3
Kipre <cyprien@kipr.cat>
2026-01-30 08:09:42 +0100

    update cade

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 import { base } from "./base.js";
-import { box } from "./box.js";
+// import { box } from "./box.js";
 // import { box } from "./test.js";
 import { Model } from "./cade/lib/lib.js";
 import { gantryPosition } from "./dimensions.js";
@@ -23,4 +23,4 @@ model.addChild(gantry, gantryPlacement.translate(gantryPosition), true);
 await model.loadMesh();
 await model.watch();
 // await model.export("C:/Users/kipr/Downloads/test.step");
-await model.project("C:/Users/kipr/Downloads/test.step");
+// await model.project("C:/Users/kipr/Downloads/test.step");
